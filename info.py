b0320973ecc93137bc147bca124523e73ee6d044
Graymatteruse <119749984+Graymatteruse@users.noreply.github.com>
2022-12-14 13:29:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ PORT = environ.get("PORT", "8080")
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://te.legra.ph/file/e78d2016bef2d419de7f2.jpg')).split()
+PICS = (environ.get('PICS', 'https://te.legra.ph/file/a3aaa2b9d2086d20700b8.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]

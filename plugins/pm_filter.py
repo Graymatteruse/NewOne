56c43e00ee741397e2be061d9f3f698c5c32bcc0
Graymatteruse <119749984+Graymatteruse@users.noreply.github.com>
2022-12-14 10:25:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -108,7 +108,7 @@ async def next_page(bot, query):
 
     btn.insert(0,
         [
-            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://telegram.me/FHDmovies24x7Official/3377')
+            InlineKeyboardButton(text="❓ How To Download ❓", url='https://telegram.me/FHDmovies24x7Official/3377')
         ]
     )
 
@@ -688,7 +688,7 @@ async def auto_filter(client, msg, spoll=False):
 
     btn.insert(0,
         [
-            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://telegram.me/FHDmovies24x7Official/3377')
+            InlineKeyboardButton(text="❓ How To Download ❓", url='https://telegram.me/FHDmovies24x7Official/3377')
         ]
     )
 
